c3c3bdca8ab006b7088070ff9ab2d35df6d4920d
JavaSandeep <saryan619sand@hotmail.com>
2017-11-14 15:14:08 +0545

    flappy bird and wierd animation

--- js-experiment/flappybird/js/script.js
+++ js-experiment/flappybird/js/script.js
@@ -1,3 +1,208 @@
+function getContainer(clasName,width,height,src)
+{
+	//GETTING CONTAINER FOR OBJECTS
+	let container = document.createElement('div');
+	
+	container.classList.add(""+clasName);
+	container.style.width=width+"px";
+	container.style.height=height+"px";
+	
+	let elem = document.createElement("img");
+	elem.setAttribute("src", "images/"+src);
+	container.appendChild(elem);
+	
+	return container;
+}
 
+class Pipe{
+	constructor(pipeContainer, xPosition, yPosition)
+	{
+		this.element=pipeContainer;
+		this.xPos=xPosition;
+		this.yVar=yPosition;
+		this.yPos=this.yVar - Math.floor(Math.random() * 160);
+		//STORING RESET VARIABLES
+		this.xPosReset=xPosition;
+		this.yVarReset=yPosition;
+	}
+	pipeReset()
+	{
+		//reset variables of pipe
+		this.xPos=this.xPosReset;
+		this.yVar=this.yVarReset;
+		this.yPos=this.yVar - Math.floor(Math.random() * 160);
+	}
+	movePipe(){
+		this.xPos-=10;//moving pipe by 10 pixel left
+	}
+	testPipe(){
+		//TESTING IF PIPE GOES BEYOND LEFT SCREEN AND GIVING PLAYER ONE POINT IF S/HE DOESN'T COLLIDE WITH THIS.PIPE
+		let gameScoreIncrementor = 0;
+		if(this.xPos<=-130)
+		{
+			this.yPos=this.yVar - Math.floor(Math.random() * 160);
+			this.xPos=1120;
+			gameScoreIncrementor = 1;
+		//move pipe
+		}
+		return gameScoreIncrementor;
+	}
+}
 
-main();
+function getPipeArray(clasName,width,height,img)
+{
+	let pipeArray = new Array();
+	for(var x = 0; x < 4; x++)
+	{
+		var pipeContainer=getContainer(clasName,width,height,img);
+		var pipe = new Pipe(pipeContainer,1120+(x*300),-70);
+		pipeArray.push(pipe);
+	}
+	return pipeArray;
+}
+
+class Frog{
+	constructor(frogContainer, xPosition, yPosition, fVelocity, iVelocity){
+		this.element=frogContainer;
+		this.xPos=xPosition;
+		this.yPos=yPosition;
+		this.yVelIntial=iVelocity;
+		this.yVelFinal=fVelocity;
+		this.isAlive=true;
+		//STORING RESET VARIABLES OF FROG
+		this.xPosReset=xPosition;
+		this.yPosReset=yPosition;
+		this.yVelIntialReset=iVelocity;
+		this.yVelFinalReset=fVelocity;
+	}
+	frogReset()
+	{
+		//reseting frog
+		this.xPos=this.xPosReset;
+		this.yPos=this.yPosReset;
+		this.yVelIntial=this.yVelIntialReset;
+		this.yVelFinal=this.yVelFinalReset;
+		this.isAlive=true;
+	}
+	frogMove(){
+		//calculating velocity v = u + at
+		this.yPos-=this.yVelFinal;
+		//negative gravity here
+		this.yVelFinal = this.yVelIntial - 1;
+		//final velocity is now initial velocity
+		this.yVelIntial = this.yVelFinal;
+		
+	}
+	frogJump(JumpForce){
+		this.yVelIntial=JumpForce//some displacement value;
+	}
+	testCollision(arrayBoundary)
+	{
+		if(this.yPos>510){return true;}
+		for(let b=0; b<arrayBoundary.length;b+=3)
+		{
+			if((this.xPos+50 > arrayBoundary[b]) && ((this.yPos-10 < arrayBoundary[b+1]) || (this.yPos+50 > arrayBoundary[b+2])))
+			{return true;}
+		}
+	}
+}
+class GameWorld{
+	constructor(frogObject, aOPipe)
+	{
+		this.frog = frogObject;
+		this.pipeArray = aOPipe;
+		this.score = 0;
+		this.gameId = 0;
+	}
+	main()
+	{
+		//GAME RUN
+		let GameScore = this.score;
+		//CANVAS START HERE
+		let	canvas = document.getElementById('myCanvas');
+		let ctx = canvas.getContext('2d');
+		ctx.font = "72px Arial";
+		
+		//BACKGROUND
+		let backgroundImage = new Image();
+		backgroundImage.src = 'images/background_scroll.jpg';
+		let offset = 0;
+		
+		let frogImage = new Image();
+		frogImage.src = (this.frog.element.getElementsByTagName('img')[0].getAttribute('src'));
+		
+		let pipeImage = new Image();
+		pipeImage.src = (this.pipeArray[0].element.getElementsByTagName('img')[0].getAttribute('src'));
+		
+		
+		this.gameId = setInterval(framme, 50,this.frog,this.pipeArray);
+		function framme(myFrog,arrayOfPipe)
+		{
+			if(!myFrog.isAlive){clearInterval(this.gameId);ctx.fillText("GAME OVER",canvas.width*0.3,canvas.height/2);}
+			else
+			{
+				//grouping functions and canvas operations together
+				//functions
+				if(offset>=1100){offset=0;}
+				myFrog.frogMove();
+				var boundaryArray= new Array();
+				//CANVAS OPERATIONS
+				ctx.clearRect(0,0,canvas.width,canvas.height);
+				for(var i = 0; i < 2; i++)
+				{
+					ctx.drawImage(backgroundImage,i*canvas.width-offset,0)
+				}
+				for(var p in arrayOfPipe)
+				{
+					ctx.drawImage(pipeImage,arrayOfPipe[p].xPos,arrayOfPipe[p].yPos);
+					arrayOfPipe[p].movePipe();
+					GameScore+=arrayOfPipe[p].testPipe();
+					if(arrayOfPipe[p].xPos<500 && arrayOfPipe[p].xPos>20)
+					{
+						boundaryArray.push(arrayOfPipe[p].xPos);
+						boundaryArray.push(arrayOfPipe[p].yPos+320);
+						boundaryArray.push(arrayOfPipe[p].yPos+560);
+					}
+				}
+				myFrog.isAlive=!myFrog.testCollision(boundaryArray);
+				
+				ctx.drawImage(frogImage,myFrog.xPos,myFrog.yPos);
+				ctx.fillText(GameScore,canvas.width*0.9,canvas.height*0.1);
+				offset+=10;
+			}
+			
+		}
+	}
+	resetWorld()
+	{
+		this.frog.frogReset();
+		for(var pipes in this.pipeArray){
+			this.pipeArray[pipes].pipeReset();
+		}
+		this.score = 0;
+		clearInterval(this.gameId);
+	}
+}
+//OUTSIDE OF INSTANCE
+let frogContainer = getContainer('frog-class',80,80,'smallfrog.png');
+let frogObject = new Frog(frogContainer,100,300,0,0);
+
+pipeArray = getPipeArray('pipe-class',125,940,'pipe_image.png');
+
+let gameWorld = new GameWorld(frogObject,pipeArray);
+gameWorld.main();
+
+let replayButton = document.getElementsByTagName("button")[0];
+
+replayButton.onmousedown = function()
+{
+	gameWorld.resetWorld();
+	gameWorld.main();
+}
+
+document.onkeydown = function(event){
+	if(gameWorld.frog.isAlive && event.keyCode==32)
+	{	
+		gameWorld.frog.frogJump(15);
+	}
+}
--- js-experiment/weird-animation/js/script.js
+++ js-experiment/weird-animation/js/script.js
@@ -4,35 +4,54 @@
 
 function main()
 {
-	var canvas = document.getElementById('myCanvas');
-	var ctx = canvas.getContext('2d');
+	let canvas = document.getElementById('myCanvas');
+	let ctx = canvas.getContext('2d');
 	//property of circle
-	var radiusOfOuterCircle = 150;
+	let trajectoryRadius = 150;
 	
 	//this has to be calculated inside for loop
 	ctx.fillStyle = '#f8948d';
-	var columLag = 0;
-	var colLag = Math.PI/36;
-	var rowLag= Math.PI/20;
-	var loopLag = Math.PI*1.2;
+	let angleWithX = 0;
+	var InColumnLag = Math.PI/36;
+	var InRowLag= Math.PI/20;
+	var eachLoopLag = Math.PI*1.2;
 	
-	var id = setInterval(framee,55);
+	let numberOfLoops = 2;
+	let rowCount = 20;
+	let columnCount = 10;
+	
+	let xMargin = 40;
+	let yMargin = 320;
+	let columnGap = 45;
+	let rowGap = 35;
+	
+	var id = setInterval(framee,60);
 	
 	function framee()
 	{
 		ctx.clearRect(0,0,canvas.width,canvas.height);
-		for(var loops = 1; loops<=2; loops++)
+		for(var loops = 1; loops<=numberOfLoops; loops++)
 		{
-			for(var j=1; j<=20;j++){
-				for(var i=1; i<=10; i++){
+			for(var j=1; j<=rowCount;j++){
+				for(var i=1; i<=columnCount; i++){
 					ctx.beginPath();
-					var cosOfSize = (((radiusOfOuterCircle+1)+(radiusOfOuterCircle)*Math.cos(columLag-(((i-1)*colLag))-((j-1)*rowLag)-((loops-1)*loopLag)))<(2*radiusOfOuterCircle-9) ? ((radiusOfOuterCircle+1)+(radiusOfOuterCircle)*Math.cos(columLag-(((i-1)*colLag))-((j-1)*rowLag)-((loops-1)*loopLag))) : (2*radiusOfOuterCircle-9));
-					ctx.arc(40+j*45, 320+i*35-(radiusOfOuterCircle+radiusOfOuterCircle*Math.sin(columLag-((j-1)*rowLag)-((loops-1)*loopLag))), (582-(2*cosOfSize))/29 , 0, Math.PI * 2, true);
+					var cosOfAngle = Math.cos(angleWithX-(((i-1)*InColumnLag))-((j-1)*InRowLag)-((loops-1)*eachLoopLag));
+					var depthFromScreen = (((trajectoryRadius+1)+(trajectoryRadius)*cosOfAngle)<(2*trajectoryRadius-9) ? ((trajectoryRadius+1)+(trajectoryRadius)*cosOfAngle) : (2*trajectoryRadius-9));
+					//depth of screen is between 1 to 290
+					var sinOfAngle = Math.sin(angleWithX-((j-1)*InRowLag)-((loops-1)*eachLoopLag))
+					var xPosition=xMargin+j*columnGap;
+					var yPosition = yMargin+i*rowGap-(trajectoryRadius+trajectoryRadius*sinOfAngle);
+					circleRadius = (582-(2*depthFromScreen))/29;
+					//circle radius is a linear funtion 
+					//if depth=1 from screen ma maximum size of circle i.e 20
+					//if depth=((2*radius)-10)+1=291 or above ma minimum size of circle i.e 0
+					//construct linear equation using (1,20)=>(x1,y1) and (291,0)=>(x2,y2)
+					ctx.arc(xPosition, yPosition, circleRadius, 0, Math.PI * 2, true);
 					ctx.fill();
 					}
 			}
 		}
-		columLag+=((Math.PI)/36);
+		angleWithX+=((Math.PI)/36);
 	}
 }
 
